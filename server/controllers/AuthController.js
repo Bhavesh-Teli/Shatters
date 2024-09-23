b08f1ee821214a82ccd76bca93d0560137321c53
Bhavesh-Teli <telibhavesh11@gmail.com>
2024-09-23 07:17:34 +0530

    first commit

---
+++ server/controllers/AuthController.js
@@ -0,0 +1,69 @@
+import jwt from "jsonwebtoken";
+import User from "../models/UserModel.js";
+import { compare } from "bcrypt";
+
+const createToken=(email,userId)=>{
+    return jwt.sign({email,userId},process.env.JWT_KEY,{expiresIn:maxAge})
+}
+
+export const signup=async (request, response, next)=>{
+    try {
+        const {email,password}=request.body;
+        if(!email || !password){
+            return response.status(400).send("Email and Password is requires.")
+        }
+        const user=await User.create({email,password});
+        response.cookie("jwt",createToken(email,user.id),{
+            maxAge,
+            secure:true,
+            sameSite:"None",
+        });
+        return response.status(201).json({
+            user:{
+            id:user.id,
+            email:user.email,
+            // firstName:user.firstName,
+            // lastName:user.lastName,
+            // image:user.image,
+            profileSetup:user.profileSetup,
+        },
+    })
+    } catch (error) {
+        console.log({error});
+        return response.status(500).send("Internal ServerError")
+    }
+} 
+
+export const login=async (request, response, next)=>{
+    try {
+        const {email,password}=request.body;
+        if(!email || !password){
+            return response.status(400).send("Email and Password is requires.")
+        }
+        const user=await User.findOne({email});
+        if(!user){
+            return response.status(404).send("User with given email is not found.");
+        }
+        const auth=await compare(password,user.password);
+        if(!auth){
+            return response.status(400).send(" Password is Incorrect.")
+
+        }
+        response.cookie("jwt",createToken(email,user.id),{
+            maxAge,
+            secure:true,
+            sameSite:"None",
+        });
+        return response.status(200).json({user:{
+            id:user.id,
+            email:user.email,
+            firstName:user.firstName,
+            lastName:user.lastName,
+            image:user.image,
+            profileSetup:user.profileSetup,
+        }})
+    } catch (error) {
+        console.log({error});
+        return response.status(500).send("Internal ServerError")
+    }
+}
